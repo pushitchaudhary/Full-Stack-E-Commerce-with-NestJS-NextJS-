a145eafb614c18813b61f371750f163e379ddfcb
Pushit Chaudhary <pushitchaudhary@Pushits-MacBook-Air.local>
2026-01-17 17:57:00 +0545

    adding ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Set a global prefix for all routes
   app.setGlobalPrefix('api/v1');
+
+  // Set Global Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: { enableImplicitConversion: true }, // Enable implicit type conversion
+    }),
+  );
 
 
   await app.listen(process.env.PORT ?? 3000);
