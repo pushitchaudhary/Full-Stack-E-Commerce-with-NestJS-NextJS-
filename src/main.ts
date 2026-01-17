969ae648b6ca8c198f6759c33cba6a532f4ffc3f
Pushit Chaudhary <pushitchaudhary@Pushits-MacBook-Air.local>
2026-01-17 18:02:07 +0545

    added cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { METHODS } from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,14 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true }, // Enable implicit type conversion
     }),
   );
+
+  // Enable CORS
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGINS?.split(',') ?? 'http://localhost:3000',
+    credentials: true,
+    method : ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE','OPTIONS'],
+    allowedHeaders : ['Content-Type', 'Authorization','Accept','Origin','X-Requested-With','Access-Control-Allow-Origin'],
+  });
 
 
   await app.listen(process.env.PORT ?? 3000);
